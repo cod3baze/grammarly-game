91a632b37c35fbba55b9afaf04f887d7b798a3c0
elias al3x <cod3er4@gmail.com>
2020-03-15 02:18:02 -0300

    Finished time function

--- src/javascript/show.js
+++ src/javascript/show.js
@@ -1,19 +1,25 @@
+'use-struct'
+
 const apk = document.querySelector("div#Apk")
 const show = document.querySelector("button#show")
+const app = document.querySelector("div#App")
 
 const test = () => {
   apk.style.display = "block"
+  app.style.visibility = "hidden"
 }
+test()
 
-setTimeout(test, 2000)
+setTimeout(test, 000)
 
 const handle_click = () => {
   const x = apk.style.display == "block" ? true : false
   if(x) {
     apk.style.display = "none"
-    console.log(apk.style.display)
+    app.style.visibility = "visible"
   }else {
     apk.style.display = "block"
+    app.style.visibility = "hidden"
     console.log(apk.style.display)
   }
 
---
+++ src/javascript/time.js
@@ -0,0 +1,33 @@
+const stamp = document.querySelector("div.game-time p#time")
+const div = document.querySelector("div.game-time")
+
+const handle_update_time = () => {
+  var stamps = {
+    hour: new Date().getHours(),
+    min: new Date().getMinutes(),
+    sec: new Date().getSeconds()
+  }
+
+  var { hour, min, sec } = stamps
+
+  var time = hour+ ":" +min+ ":"+ sec
+
+  stamp.innerHTML = time
+  
+  if(min >= 50) clearInterval(id)
+
+  if (min >= 1) {
+    div.style.backgroundColor = "red";
+    div.style.color = "white";
+  }
+}
+
+const id = setInterval(handle_update_time, 1000)
+
+const result = {
+  id: 0,
+  tmp: 0,
+  pontuacao: 0,
+  certas: 0,
+  erradas: 0
+}
